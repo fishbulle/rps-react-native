bc153618bdfab8ae4ad728ceab4024667619e691
Angelina <linaemalmros@gmail.com>
2023-05-23 09:54:03 +0200

    wip navigator + screens

---
+++ components/Nav.tsx
@@ -0,0 +1,44 @@
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { Login } from "../screens/Login"
+import { Home } from "../screens/Home"
+import { Game } from "../screens/Game"
+import { OpenGames } from "../screens/OpenGames"
+
+export function Nav() {
+    const Stack = createNativeStackNavigator()
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="Login"
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: '#fff'
+                    },
+                    headerTintColor: '#000',
+                    headerTitleStyle: {
+                        fontSize: 30,
+                        color: '#571d0b',
+                        fontFamily: 'monospace',
+                        fontWeight: 'bold'
+                    },
+                    headerTitleAlign: 'center'
+
+                }}>
+                <Stack.Screen
+                    options={{ headerShown: false }}
+                    name='Login' component={Login} />
+                <Stack.Screen
+                    options={{ headerShown: false }} 
+                    name='Home' component={Home} />
+                <Stack.Screen
+                    options={{ headerShown: false }}
+                    name='Game' component={Game} />
+                <Stack.Screen
+                    options={{ headerShown: false }}
+                    name='OpenGames' component={OpenGames} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
---
+++ screens/OpenGames.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+export function OpenGames() {
+    return (
+        <View>
+
+        </View>
+    )
+}
