ecbf96f8b657938ee86a81953600a884a324fd34
Angelina <linaemalmros@gmail.com>
2023-06-01 09:43:03 +0200

    la till touchableWithoutFeedback på login sidan

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,12 +1,18 @@
 import { PlayerName } from "../components/PlayerName";
 import { Header } from "../components/Header";
 import { Background } from "../components/BackgroundImg";
+import { Keyboard, TouchableWithoutFeedback } from "react-native";
 
 export function Login() {
     return (
-        <Background>
-            <Header />
-            <PlayerName />
-        </Background>
+        <TouchableWithoutFeedback
+            onPress={() => {
+                Keyboard.dismiss()
+            }}>
+            <Background>
+                <Header />
+                <PlayerName />
+            </Background>
+        </TouchableWithoutFeedback>
     )
 }
